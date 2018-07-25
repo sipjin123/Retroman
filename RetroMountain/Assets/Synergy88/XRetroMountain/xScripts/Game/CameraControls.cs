d68ee1fe511c1fe29afc44baa176afbd36174e09
Burlin <burlin.gauna@synergy88digital.com>
2018-07-25 18:53:04 +0800

    file sort. pre merge with framework

---
+++ RetroMountain/Assets/Synergy88/XRetroMountain/xScripts/Game/CameraControls.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraControls : MonoBehaviour {
+	private static CameraControls _instance;
+	public static CameraControls Instance { get { return _instance; } }
+
+	public GameObject PlayerObject;
+	public float[] RandomizedRotationAxis;
+	public GameObject _LeftPosition, _RightPosition;
+	public GameObject _CamObj;
+
+	bool _lerpToCamSwitch;
+	Vector3  _oBjToLerpTo, _objtoLerpFrom;
+
+	float _lerpSpeed;
+	public bool _startFollow;
+
+	void Awake()
+	{
+		_instance = this;
+	}
+	void Start () 
+	{
+		_lerpToCamSwitch = false;
+		_startFollow = false;
+	}
+
+	void FixedUpdate () {
+		if(_startFollow)
+		transform.position = new Vector3 ( PlayerObject.transform.position.x, transform.position.y, PlayerObject.transform.position.z );
+	
+		if(_lerpToCamSwitch)
+		{
+			LerpThoThisCam();
+		}
+	}
+	public void _ResetToDirection(int _direction)
+	{
+		_lerpToCamSwitch = false;
+		_lerpValue = 0;
+		SwitchCam(_direction);
+	}
+	public void SwitchCam(int _direction)
+	{
+		if(_direction == 180 )
+		{
+			_oBjToLerpTo = _LeftPosition.transform.localPosition;
+			_objtoLerpFrom = _CamObj.transform.localPosition;
+			_lerpToCamSwitch = true;
+			_lerpSpeed =  1f;
+		}
+		else if(_direction == 0)
+		{
+			_oBjToLerpTo = _RightPosition.transform.localPosition;
+			_objtoLerpFrom = _CamObj.transform.localPosition;
+			_lerpToCamSwitch = true;
+			_lerpSpeed =  1f;
+		}
+	}
+
+	float _lerpValue;
+	public void LerpThoThisCam()
+	{
+		if(_lerpValue < 1)
+		{
+			_lerpValue += _lerpSpeed * Time.deltaTime;	
+			_CamObj.transform.localPosition = Vector3.Lerp(_objtoLerpFrom, _oBjToLerpTo, _lerpValue);
+		}
+		else
+		{
+			_lerpToCamSwitch = false;
+
+			_lerpValue = 0;
+			return;
+		}
+	}
+}
---
+++ RetroMountain/Assets/Synergy88/XRetroMountain/xScripts/Platform/PlatformMinion.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMinion : MonoBehaviour {
+
+
+	public enum TypeofPlatform
+	{
+		NORMAL,
+		SPIKED,
+		HOLED,
+		UPSPIKE,
+		LOWHOLE,
+		UPGROUND,
+		LOWGROUND,
+		COINBOX,
+		LEFT,
+		RIGHT,
+		UNKNOWN
+	}
+	public TypeofPlatform _typeOfPlatform;
+	public TypeofPlatform _previousTypeOfPlatform;
+	public enum HeightOfPlatform
+	{
+		LOW,
+		NORMAL,
+		HIGH,
+		VERYHIGH
+	}
+	public HeightOfPlatform _heightOfPlatform;
+
+
+
+	public GameObject _meshParent;
+	public GameObject _trapParent;
+
+	public GameObject  _spikeObject, _holeObject, _coinBoxObject, _coinBoxMesh, _coinBoxEffects, _leftObject, _rightObject;
+	public GameObject _higherPlatform, _lowerPlatform;
+
+	public GameObject _treeObject, _shadowObject;
+
+	public bool _hasTree;
+
+
+	BoxCollider _boxCollider;
+	void Awake()
+	{
+		_boxCollider = GetComponent<BoxCollider>();
+	}
+	public void UpdateThisPlatform()
+	{
+		DisableAll();
+		switch(_typeOfPlatform)
+		{
+			case TypeofPlatform.UNKNOWN:
+				_hasTree = false;
+				return;
+				break;
+			case TypeofPlatform.SPIKED:
+				_spikeObject.SetActive(true);
+				break;
+			case TypeofPlatform.HOLED:
+				_holeObject.SetActive(true);
+				_meshParent.SetActive(false);
+				_boxCollider.enabled = false;
+				break;
+			case TypeofPlatform.UPSPIKE:
+				_spikeObject.SetActive(true);
+				break;
+			case TypeofPlatform.LOWHOLE:
+				_holeObject.SetActive(true);
+				_meshParent.SetActive(false);
+				GetComponent<BoxCollider>().enabled = false;
+				break;
+			case TypeofPlatform.COINBOX:
+				_coinBoxObject.SetActive(true);
+				_coinBoxMesh.SetActive(true);
+				_coinBoxEffects.SetActive(false);
+				break;
+			case TypeofPlatform.LEFT:
+				_leftObject.SetActive(true);
+				break;
+			case TypeofPlatform.RIGHT:
+				_rightObject.SetActive(true);
+				break;
+		}
+
+		switch(_heightOfPlatform)
+		{
+			case HeightOfPlatform.LOW:
+				_lowerPlatform.SetActive(true);
+				transform.position = new Vector3( transform.position.x, -1.5f, transform.position.z);
+				break;
+			case HeightOfPlatform.NORMAL:
+				_higherPlatform.SetActive(true);
+				transform.position = new Vector3( transform.position.x, 0, transform.position.z);
+				break;
+			case HeightOfPlatform.HIGH:
+				_higherPlatform.SetActive(true);
+				transform.position = new Vector3( transform.position.x, 1.5f, transform.position.z);
+				break;
+			case HeightOfPlatform.VERYHIGH:
+				_lowerPlatform.SetActive(true);
+				transform.position = new Vector3( transform.position.x, 3, transform.position.z);
+				break;
+		}
+
+		if(_typeOfPlatform == TypeofPlatform.UPGROUND || _typeOfPlatform == TypeofPlatform.UPSPIKE)
+		{
+			transform.position = new Vector3( transform.position.x, transform.position.y +1.5f, transform.position.z);
+		}
+		else if(_typeOfPlatform == TypeofPlatform.LOWGROUND || _typeOfPlatform == TypeofPlatform.LOWHOLE)
+		{
+			transform.position = new Vector3( transform.position.x, transform.position.y -1.5f, transform.position.z);
+		}
+
+		_previousTypeOfPlatform = _typeOfPlatform;
+		_treeObject.SetActive(_hasTree);
+
+		_shadowObject.transform.position = new Vector3 ( _shadowObject.transform.position.x, -3 , _shadowObject.transform.position.z );
+	}
+	public void DisableAll()
+	{
+		switch(_previousTypeOfPlatform)
+		{
+			case TypeofPlatform.SPIKED:
+				_spikeObject.SetActive(false);
+				break;
+			case TypeofPlatform.UPSPIKE:
+				_spikeObject.SetActive(false);
+				break;
+			case TypeofPlatform.HOLED:
+				_meshParent.SetActive(true);
+				_boxCollider.enabled = true;
+				_holeObject.SetActive(false);
+				break;
+			case TypeofPlatform.LOWHOLE:
+				_meshParent.SetActive(true);
+				_boxCollider.enabled = true;
+				_holeObject.SetActive(false);
+				break;
+			case TypeofPlatform.COINBOX:
+				_coinBoxMesh.SetActive(false);
+				_coinBoxEffects.SetActive(false);
+				_coinBoxObject.SetActive(false);
+				break;
+			case TypeofPlatform.LEFT:
+				_leftObject.SetActive(false);
+				break;
+			case TypeofPlatform.RIGHT:
+				_rightObject.SetActive(false);
+				break;
+		}
+		_higherPlatform.SetActive(false);
+		_lowerPlatform.SetActive(false);
+
+
+
+	}
+}
