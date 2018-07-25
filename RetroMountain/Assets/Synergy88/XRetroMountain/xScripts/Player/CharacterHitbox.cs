bbef20b8bd1677306789144ad35716776d9a153c
Burlin Gauna <burlin_gauna@yahoo.com>
2018-07-25 18:53:04 +0800

    file sort. pre merge with framework

---
+++ RetroMountain/Assets/Synergy88/XRetroMountain/xScripts/Player/CharacterHitbox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHitbox : MonoBehaviour {
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if(hit.gameObject.tag == "Ground")
+		{
+			GameControls.Instance.GameOverIT();
+		}
+	}
+}
