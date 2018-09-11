adb1a0d7642d11ea53d8be5d3ec4706891018c2d
Burlin <burlin.gauna@synergy88digital.com>
2018-09-11 15:32:53 +0800

    bug still exists(collider passes through ground) temporary fix refreshing collider upon generation of platform

--- Framework/Assets/RETRO/XRetroMountain/xScripts/Player/CharacterHitbox.cs
+++ Framework/Assets/RETRO/XRetroMountain/xScripts/Player/CharacterHitbox.cs
@@ -3,21 +3,35 @@ using System.Collections;
 using Common.Utils;
 using Retroman;
 
-public class CharacterHitbox : MonoBehaviour {
+public class CharacterHitbox : MonoBehaviour
+{
 
     void OnTriggerStay(Collider hit)
     {
-        Debug.LogError("Character STAY Hitbox Colliding with :: " + hit.gameObject.name + " " + LayerMask.LayerToName(hit.gameObject.layer));
+        return;
+       // Debug.LogError("Character STAY Hitbox Colliding with :: " + hit.gameObject.name + " " + LayerMask.LayerToName(hit.gameObject.layer));
 
     }
     void OnTriggerEnter(Collider hit)
-	{
-        Debug.LogError("Character Hitbox Colliding with :: " + hit.gameObject.name + " " + LayerMask.LayerToName(hit.gameObject.layer));
+    {
+       // return;
+       // Debug.LogError("Character Hitbox Colliding with :: " + hit.gameObject.name + " " + LayerMask.LayerToName(hit.gameObject.layer));
 
-        if (LayerMask.LayerToName( hit.gameObject.layer) == "GroundOnly")
-		{
+        if(hit.GetComponent<PlatformMinion>() != null)//if (LayerMask.LayerToName(hit.gameObject.layer) == "GroundOnly")
+        {
             Factory.Get<VFXHandler>().RequestVFX(hit.transform.position, VFXHandler.VFXList.BumpVFX);
             Factory.Get<DataManagerService>().MessageBroker.Publish(new GameOverSignal());
-		}
-	}
+        }
+    }
+    void OnCollisionEnter(Collision hit)
+    {
+        return;
+        //Debug.LogError("Character Hitbox COLL with :: " + hit.gameObject.name + " " + LayerMask.LayerToName(hit.gameObject.layer));
+
+        if(hit.gameObject.GetComponent<PlatformMinion>() != null)//if (LayerMask.LayerToName(hit.gameObject.layer) == "GroundOnly")
+        {
+            Factory.Get<VFXHandler>().RequestVFX(hit.transform.position, VFXHandler.VFXList.BumpVFX);
+            Factory.Get<DataManagerService>().MessageBroker.Publish(new GameOverSignal());
+        }
+    }
 }
